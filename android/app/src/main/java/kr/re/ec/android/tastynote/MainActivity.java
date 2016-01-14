8dce1fa096eda4b3d5ed0f822eef4eac47eb1038
Taehee Kim <slhyvaa@nate.com>
2016-01-14 14:50:42 +0900

    apply Google Calendar API and create SampleActivity

--- android/app/src/main/java/kr/re/ec/android/tastynote/MainActivity.java
+++ android/app/src/main/java/kr/re/ec/android/tastynote/MainActivity.java
@@ -1,13 +1,26 @@
 package kr.re.ec.android.tastynote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** TEMP: must be public */
+    public void showCalendar(View view) {
+        Log.d(TAG, "showCalendar clicked");
+        Intent intent = new Intent(this, GoogleCalendarSampleActivity.class);
+        startActivity(intent);
+
     }
 }
