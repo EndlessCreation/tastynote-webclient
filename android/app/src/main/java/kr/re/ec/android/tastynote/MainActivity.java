29dbac212e2a56f3482f52bfb069f8ff8505c7da
Taehee Kim <slhyvaa@nate.com>
2016-01-14 14:16:07 +0900

    android first commit

---
+++ android/app/src/main/java/kr/re/ec/android/tastynote/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.re.ec.android.tastynote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
