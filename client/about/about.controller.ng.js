ea9bd86f3c8e9cf1f19562fede602a72bdea9241
Kang Juho <rjduakcjs@gmail.com>
2016-03-12 22:21:58 -0800

    Got something from the google drive api(/about)
    but i don't have a single clue how to use this.

--- client/about/about.controller.ng.js
+++ client/about/about.controller.ng.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('tastynoteApp')
-.controller('AboutCtrl', function($scope) {
-  $scope.viewName = 'About';
-});
+    .controller('AboutCtrl', function ($scope, $http) {
+        var req={
+            method : 'GET',
+            url:'https://www.googleapis.com/drive/v3/about'
+        };
+        $http.get(req).then(function(response){
+            $scope.aboveData = response;
+            console.log(response);
+        });
+        $scope.viewName = 'About';
+    });
